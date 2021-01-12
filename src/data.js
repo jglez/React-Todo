6dff9889672b36bab296eddb511868e07aa86a4e
jglez <jglez.cs@gmail.com>
2021-01-11 18:10:51 -0600

    Created todo task data

---
+++ src/data.js
@@ -0,0 +1,33 @@
+// TO DO DATA
+const data = [
+  {
+    task: 'Wash dishes',
+    id: 0,
+    completed: false
+  },
+  {
+    task: 'Walk dog',
+    id: 1,
+    completed: false
+  },
+  {
+    task: 'Make dinner',
+    id: 2,
+    completed: false
+  },
+  {
+    task: 'Get groceries',
+    id: 3,
+    completed: false
+  },
+  {
+    task: 'Do homework',
+    id: 4,
+    completed: false
+  },
+  {
+    task: 'Study',
+    id: 5,
+    completed: false
+  }
+]
